138e77ec7e97a65e8694750f7faf5a31cc334cad
Nathan Jones <njonessurvey@gmail.com>
2024-03-15 18:17:15 -0500

    convert all images to webp format to save space

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./styles.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const icons = document.querySelectorAll(".icon");
+  icons.forEach((icon) => {
+    if (icon.closest("a").visited) {
+      icon.style.fill = "purple"; // Change the color for visited links
+    } else {
+      icon.style.fill = "black"; // Default color for unvisited links
+    }
+  });
+});
